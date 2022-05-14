404263fcf33be1642ba3ee2cf32e366e71873d0e
DanGoer <87978392+DanGoer@users.noreply.github.com>
2022-05-14 12:10:59 +0200

    reworked gallery route and BE cleanup

--- server/routes/gallery.js
+++ server/routes/gallery.js
@@ -6,68 +6,53 @@ const Router = express.Router();
 const fs = require("fs");
 
 //Create image at MongoDB
-Router.post(
-  "/",
-  async (req, res) => {
-    const auth = req.currentUser;
-    const photo = new Photo(req.body);
-    if (auth) {
-      try {
-        const savedPhoto = await photo.save();
-        res.status(200).json(savedPhoto);
-      } catch (err) {
-        res.status(500).send({
-          upload_error: "Error while uploading file...Try again later.",
-        });
-      }
-    } else res.status(403).send("Not authorized");
-  },
-  (err, req, res, next) => {
-    if (err) {
-      res.status(500).send({
-        upload_error: error.message,
-      });
-    }
+Router.post("/", async (req, res) => {
+  const auth = req.currentUser;
+  const photo = new Photo(req.body);
+  if (!auth) res.status(403).send("Nicht autorisiert!");
+  try {
+    const savedPhoto = await photo.save();
+
+    res.status(200).json(savedPhoto);
+  } catch (err) {
+    res.status(500).json(err);
   }
-);
+});
 
 //Delete image from MongoDB and server
 Router.delete("/:id", async (req, res) => {
   const auth = req.currentUser;
-  console.log(req.currentUser);
-  if (auth) {
-    try {
-      const photo = await Photo.findById(req.params.id);
-      if (photo.username === req.body.username) {
-        try {
-          const path = `./galleryimages/${photo.photo}`;
-          if (fs.existsSync(path)) {
-            fs.unlink(path, function (err) {
-              if (err) throw err;
-            });
-          }
-          await photo.delete();
-          res.status(200).json("Photo deleted...");
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      } else {
-        res.status(401).json("You can only delete your own photos!");
-      }
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  } else res.status(403).send("Not authorized");
+  if (!auth) res.status(403).send("Nicht autorisiert!");
+  try {
+    const photo = await Photo.findById(req.params.id);
+    if (photo.username !== req.body.username)
+      res.status(401).json("Es können nur die eigenen Fotos gelöscht werden!");
+
+    const path = `./galleryimages/${photo.photo}`;
+    if (!fs.existsSync(path))
+      res.status(403).send("Die Datei existiert nicht!");
+
+    fs.unlink(path, function (err) {
+      if (err) throw err;
+    });
+    await photo.delete();
+
+    res.status(200).json("Foto gelöscht...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //Read imagelist from MongoDB
 Router.get("/photos", async (req, res) => {
   try {
     const photos = await Photo.find({});
+
     photos.reverse();
+
     res.send(photos);
   } catch (error) {
-    res.status(500).send({ get_error: "Error while getting list of photos." });
+    res.status(500).json(err);
   }
 });
 
