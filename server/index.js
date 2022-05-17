e841f3a8546f5c8067e530d85854fdc9afe0a415
DanGoer <87978392+DanGoer@users.noreply.github.com>
2022-05-17 11:30:33 +0200

    fixed routing problem

--- server/index.js
+++ server/index.js
@@ -30,15 +30,15 @@ mongoose
   .catch((err) => console.log(err));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
 
-  // index.html for all page routes
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
-}
+app.use(express.static("client/build"));
+
+// index.html for all page routes
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "../client", "build", "public", "index.html")
+  );
+});
 
 //ContactRoute
 app.use("/api/contact", contactRoute);
